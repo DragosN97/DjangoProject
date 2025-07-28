499e404c91f0714fce002b387daf59d81ba2585e
DragosN97 <dragos.orasanu9@gmail.com>
2025-07-28 15:01:42 +0300

    Modifications after last 2 courses

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from books import views
+from books.views import BookViewSet
+
 
 router = DefaultRouter()
 router.register('books', views.BookViewSet)
@@ -11,7 +13,8 @@ urlpatterns = [
     path('hello/', views.hello, name='hello'),
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    #path('create_book', views.create_book, name = 'create_book'),
     path('api/', include(router.urls)),
+
+    path('books/', views.books_view_c, name='books'),
 
 ]
--- books/views.py
+++ books/views.py
@@ -1,9 +1,17 @@
+import json
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from datetime import datetime
+
+from django.template.context_processors import request
+
+from books.forms import BookForm
 from books.models import Book
 from rest_framework import viewsets
 from books.serializers import BookSerializers
+from django.views.decorators.csrf import csrf_exempt
+from books.forms import BookForm
 
 
 # Create your views here.
@@ -33,3 +41,35 @@ def about(request):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
+
+book_list = ["Book1", "Mockingbirds", "Ex1"]
+
+@csrf_exempt
+def book_view(request: HttpRequest):
+    if request.method == "GET":
+        book_list.sort()
+        context = {
+            "books": book_list
+        }
+        return render(request, 'books.html', context)
+    elif request.method == "POST":
+        data = json.loads(request.body)
+        book_title = data["title"]
+        book_list.append(book_title)
+        return HttpResponse("")
+
+@csrf_exempt
+def books_view_c(request: HttpRequest):
+    if request.method == "GET":
+        context = {
+            'form': BookForm()
+
+        }
+        return render(request, 'create_book.html', context)
+    elif request.method == "POST":
+        form_with_data = BookForm(request.POST)
+        if form_with_data.is_valid():
+            form_with_data.save()
+            return HttpResponse("Book save successfully!")
+        else:
+            return HttpResponse(form_with_data.errors)
